5d5ffd879145ca0cdbd012ce77df9922836bba65
Danilo Tosev <70350069+xtosev@users.noreply.github.com>
2024-05-30 12:01:55 +0200

    Dodal za sifratxtbox da ne enable=false

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,26 +35,26 @@
             this.tstripOAplikaciji = new System.Windows.Forms.ToolStripButton();
             this.tstripIzlaz = new System.Windows.Forms.ToolStripButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.tbSifra = new System.Windows.Forms.TextBox();
-            this.tbIme = new System.Windows.Forms.TextBox();
-            this.tbPrezime = new System.Windows.Forms.TextBox();
-            this.tbAdresa = new System.Windows.Forms.TextBox();
-            this.cbGrad = new System.Windows.Forms.ComboBox();
-            this.tbTelefon = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbTelefon = new System.Windows.Forms.TextBox();
+            this.cbGrad = new System.Windows.Forms.ComboBox();
+            this.tbAdresa = new System.Windows.Forms.TextBox();
+            this.tbPrezime = new System.Windows.Forms.TextBox();
+            this.tbIme = new System.Windows.Forms.TextBox();
+            this.tbSifra = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.toolStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -73,6 +73,7 @@
             this.toolStrip1.Size = new System.Drawing.Size(1161, 62);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
             // 
             // tstripIzmeni
             // 
@@ -137,6 +138,105 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(33, 221);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Telefon";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Grad";
+            // 
+            // tbTelefon
+            // 
+            this.tbTelefon.Location = new System.Drawing.Point(88, 218);
+            this.tbTelefon.Name = "tbTelefon";
+            this.tbTelefon.Size = new System.Drawing.Size(171, 20);
+            this.tbTelefon.TabIndex = 9;
+            // 
+            // cbGrad
+            // 
+            this.cbGrad.FormattingEnabled = true;
+            this.cbGrad.Location = new System.Drawing.Point(88, 190);
+            this.cbGrad.Name = "cbGrad";
+            this.cbGrad.Size = new System.Drawing.Size(209, 21);
+            this.cbGrad.TabIndex = 8;
+            // 
+            // tbAdresa
+            // 
+            this.tbAdresa.Location = new System.Drawing.Point(88, 163);
+            this.tbAdresa.Name = "tbAdresa";
+            this.tbAdresa.Size = new System.Drawing.Size(253, 20);
+            this.tbAdresa.TabIndex = 7;
+            // 
+            // tbPrezime
+            // 
+            this.tbPrezime.Location = new System.Drawing.Point(88, 135);
+            this.tbPrezime.Name = "tbPrezime";
+            this.tbPrezime.Size = new System.Drawing.Size(209, 20);
+            this.tbPrezime.TabIndex = 6;
+            // 
+            // tbIme
+            // 
+            this.tbIme.Location = new System.Drawing.Point(88, 110);
+            this.tbIme.Name = "tbIme";
+            this.tbIme.Size = new System.Drawing.Size(209, 20);
+            this.tbIme.TabIndex = 5;
+            // 
+            // tbSifra
+            // 
+            this.tbSifra.Enabled = false;
+            this.tbSifra.Location = new System.Drawing.Point(88, 83);
+            this.tbSifra.Name = "tbSifra";
+            this.tbSifra.ReadOnly = true;
+            this.tbSifra.Size = new System.Drawing.Size(100, 20);
+            this.tbSifra.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Adresa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Prezime";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ime";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sifra";
+            // 
             // groupBox2
             // 
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -156,150 +256,14 @@
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             // 
-            // listBox1
+            // label13
             // 
-            this.listBox1.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 11;
-            this.listBox1.Location = new System.Drawing.Point(0, 66);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(770, 268);
-            this.listBox1.TabIndex = 0;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(49, 86);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(28, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Sifra";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(53, 113);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(24, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Ime";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(33, 138);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Prezime";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(37, 166);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(40, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Adresa";
-            // 
-            // tbSifra
-            // 
-            this.tbSifra.Location = new System.Drawing.Point(88, 83);
-            this.tbSifra.Name = "tbSifra";
-            this.tbSifra.ReadOnly = true;
-            this.tbSifra.Size = new System.Drawing.Size(100, 20);
-            this.tbSifra.TabIndex = 4;
-            // 
-            // tbIme
-            // 
-            this.tbIme.Location = new System.Drawing.Point(88, 110);
-            this.tbIme.Name = "tbIme";
-            this.tbIme.Size = new System.Drawing.Size(209, 20);
-            this.tbIme.TabIndex = 5;
-            // 
-            // tbPrezime
-            // 
-            this.tbPrezime.Location = new System.Drawing.Point(88, 135);
-            this.tbPrezime.Name = "tbPrezime";
-            this.tbPrezime.Size = new System.Drawing.Size(209, 20);
-            this.tbPrezime.TabIndex = 6;
-            // 
-            // tbAdresa
-            // 
-            this.tbAdresa.Location = new System.Drawing.Point(88, 163);
-            this.tbAdresa.Name = "tbAdresa";
-            this.tbAdresa.Size = new System.Drawing.Size(253, 20);
-            this.tbAdresa.TabIndex = 7;
-            // 
-            // cbGrad
-            // 
-            this.cbGrad.FormattingEnabled = true;
-            this.cbGrad.Location = new System.Drawing.Point(88, 190);
-            this.cbGrad.Name = "cbGrad";
-            this.cbGrad.Size = new System.Drawing.Size(209, 21);
-            this.cbGrad.TabIndex = 8;
-            // 
-            // tbTelefon
-            // 
-            this.tbTelefon.Location = new System.Drawing.Point(88, 218);
-            this.tbTelefon.Name = "tbTelefon";
-            this.tbTelefon.Size = new System.Drawing.Size(171, 20);
-            this.tbTelefon.TabIndex = 9;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(47, 193);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(30, 13);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Grad";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(33, 221);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(43, 13);
-            this.label6.TabIndex = 11;
-            this.label6.Text = "Telefon";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(6, 50);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(28, 13);
-            this.label7.TabIndex = 12;
-            this.label7.Text = "Sifra";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(282, 50);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(40, 13);
-            this.label8.TabIndex = 12;
-            this.label8.Text = "Adresa";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(61, 50);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(24, 13);
-            this.label9.TabIndex = 13;
-            this.label9.Text = "Ime";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(169, 50);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(44, 13);
-            this.label11.TabIndex = 15;
-            this.label11.Text = "Prezime";
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(577, 51);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(43, 13);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "Telefon";
             // 
             // label12
             // 
@@ -310,14 +274,52 @@
             this.label12.TabIndex = 16;
             this.label12.Text = "Grad";
             // 
-            // label13
+            // label8
             // 
-            this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(577, 51);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(43, 13);
-            this.label13.TabIndex = 17;
-            this.label13.Text = "Telefon";
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(282, 50);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Adresa";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(169, 50);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(44, 13);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "Prezime";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(61, 50);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(24, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Ime";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 50);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Sifra";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 11;
+            this.listBox1.Location = new System.Drawing.Point(0, 66);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(770, 268);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
